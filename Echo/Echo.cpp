ad94459b47bc75c246a55fdafed4682662866e08
Andrew Daume <98475588+ADaume@users.noreply.github.com>
2022-01-26 16:02:26 +0000

    Added echo.cpp SO cool :)

---
+++ Echo/Echo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::string input
+  std::getline(std::cin, input);
+  cout << input << '\n';
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is my code repository for NHS computer science!!1!!1!!
 This is my first gibhub x replit edit! YEA EPIC!!
 
 
+SO ciil!
